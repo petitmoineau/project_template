f659a2ed127d4c5c982e0f3ac233e61c15e61874
Anastasiia Parkhomenko <anastasia031100@gmail.com>
2024-03-15 17:29:49 +0200

    added empty functions for input and output

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def console_input():
+    """reads input from the console"""
+    pass
+
+def read_from_file(filepath):
+    """reads content from a file using built-in python functions"""
+    pass
+
+def read_from_file_pandas(filepath):
+    """reads content from a file using the pandas library"""
+    pass
